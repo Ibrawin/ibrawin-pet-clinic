60a1b588ef48445dd9a289340ea9f052575b8efb
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-18 04:25:04 +0000

    Task 4: Create interfaces the service layer

---
+++ pet-clinic-data/src/main/java/com/ibrawin/petclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package com.ibrawin.petclinic.service;
+
+import com.ibrawin.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
