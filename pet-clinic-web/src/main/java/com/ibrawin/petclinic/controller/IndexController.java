0ded2968e807ad468cfa67cd64fd3bdd97eef047
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-21 20:06:58 +0000

    Task 7: Add Pet Clinic Index Page and Controller

---
+++ pet-clinic-web/src/main/java/com/ibrawin/petclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.ibrawin.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
