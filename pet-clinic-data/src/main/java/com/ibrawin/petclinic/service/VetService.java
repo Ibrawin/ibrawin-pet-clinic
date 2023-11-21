07b3fe1fafe51fe5de68c6beaa8d761e19eb0e36
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-21 00:10:39 +0000

    Task 11: Refactoring to common interface.

--- pet-clinic-data/src/main/java/com/ibrawin/petclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/ibrawin/petclinic/service/VetService.java
@@ -2,13 +2,6 @@ package com.ibrawin.petclinic.service;
 
 import com.ibrawin.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
