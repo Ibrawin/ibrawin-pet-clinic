2113dc0e9e899071d7ce5f9ca38ea4f8d602d868
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-21 19:11:55 +0000

    Task 5: Implement Map Based Services

---
+++ pet-clinic-data/src/main/java/com/ibrawin/petclinic/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.ibrawin.petclinic.service.map;
+
+import com.ibrawin.petclinic.model.Vet;
+import com.ibrawin.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
