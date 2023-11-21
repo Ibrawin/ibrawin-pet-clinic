f5bdb757ae1380bbb51057b4eb0cf62c995eaaa9
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-21 20:29:27 +0000

    Task 12: Create Owner Index Page and Controller

---
+++ pet-clinic-web/src/main/java/com/ibrawin/petclinic/controller/OwnerController.java
@@ -0,0 +1,13 @@
+package com.ibrawin.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping("/owners")
+    public String owners() {
+        return "owners";
+    }
+}
