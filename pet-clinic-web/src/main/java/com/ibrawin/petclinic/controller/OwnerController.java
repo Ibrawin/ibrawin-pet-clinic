cb9b719c35aae5d9559212b7812684608072b01a
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-22 13:35:19 +0000

    Task 15: List all Owners on Owner Page

--- pet-clinic-web/src/main/java/com/ibrawin/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ibrawin/petclinic/controller/OwnerController.java
@@ -1,13 +1,22 @@
 package com.ibrawin.petclinic.controller;
 
+import com.ibrawin.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping("/owners")
-    public String owners() {
+    public String owners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners";
     }
 }
