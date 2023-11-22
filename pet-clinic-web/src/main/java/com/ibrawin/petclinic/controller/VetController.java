502df0d4f1a02fee2012175d2ceb501d12b0ec24
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-22 13:50:06 +0000

    Task 14: List all Vets on Vet Index page

--- pet-clinic-web/src/main/java/com/ibrawin/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/ibrawin/petclinic/controller/VetController.java
@@ -1,13 +1,22 @@
 package com.ibrawin.petclinic.controller;
 
+import com.ibrawin.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
-    public String vets() {
+    public String vets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets";
     }
 }
